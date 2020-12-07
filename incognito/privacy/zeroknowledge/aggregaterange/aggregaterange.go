1bd38b674062c454658943c3f7bbfca7890bc203
hiennguyen-me <nguyenthithuhien249@gmail.com>
2020-12-07 16:43:09 +0700

    Revert old bullet proofs

--- incognito/privacy/zeroknowledge/aggregaterange/aggregaterange.go
+++ incognito/privacy/zeroknowledge/aggregaterange/aggregaterange.go
@@ -447,21 +447,259 @@ func (wit AggregatedRangeWitness) ProveOld() (*AggregatedRangeProof, error) {
 	return proof, nil
 }
 
+// revert to old proving
 func (wit AggregatedRangeWitness) Prove() (*AggregatedRangeProof, error) {
-	wit2 := new(bulletproofs.AggregatedRangeWitness)
-	wit2.Set(wit.values, wit.rands)
-
-	proof2, err := wit2.Prove()
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("cannot prove bulletproof v2. Error %v", err))
-	}
-	proof2Bytes := proof2.Bytes()
 	proof := new(AggregatedRangeProof)
-	err = proof.SetBytes(proof2Bytes)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return nil, errors.New(fmt.Sprintf("cannot convert proof ver 2  to ver 1. Error %v", err))
+
+	numValue := len(wit.values)
+	if numValue > maxOutputNumber {
+		return nil, errors.New("Must less than maxOutputNumber")
 	}
+	numValuePad := pad(numValue)
+	aggParam := new(bulletproofParams)
+	aggParam.g = AggParam.g[0 : numValuePad*maxExp]
+	aggParam.h = AggParam.h[0 : numValuePad*maxExp]
+	aggParam.u = AggParam.u
+	csByteH := []byte{}
+	csByteG := []byte{}
+	for i := 0; i < len(aggParam.g); i++ {
+		csByteG = append(csByteG, aggParam.g[i].ToBytesS()...)
+		csByteH = append(csByteH, aggParam.h[i].ToBytesS()...)
+	}
+	aggParam.cs = append(aggParam.cs, csByteG...)
+	aggParam.cs = append(aggParam.cs, csByteH...)
+	aggParam.cs = append(aggParam.cs, aggParam.u.ToBytesS()...)
+
+	values := make([]uint64, numValuePad)
+	rands := make([]*privacy.Scalar, numValuePad)
+
+	for i := range wit.values {
+		values[i] = wit.values[i]
+		rands[i] = new(privacy.Scalar).Set(wit.rands[i])
+	}
+
+	for i := numValue; i < numValuePad; i++ {
+		values[i] = uint64(0)
+		rands[i] = new(privacy.Scalar).FromUint64(0)
+	}
+
+	proof.cmsValue = make([]*privacy.Point, numValue)
+	for i := 0; i < numValue; i++ {
+		proof.cmsValue[i] = privacy.PedCom.CommitAtIndex(new(privacy.Scalar).FromUint64(values[i]), rands[i], privacy.PedersenValueIndex)
+	}
+
+	n := maxExp
+	// Convert values to binary array
+	aL := make([]*privacy.Scalar, numValuePad*n)
+	for i, value := range values {
+		tmp := privacy.ConvertUint64ToBinary(value, n)
+		for j := 0; j < n; j++ {
+			aL[i*n+j] = tmp[j]
+		}
+	}
+
+	twoNumber := new(privacy.Scalar).FromUint64(2)
+	twoVectorN := powerVector(twoNumber, n)
+
+	aR := make([]*privacy.Scalar, numValuePad*n)
+
+	for i := 0; i < numValuePad*n; i++ {
+		aR[i] = new(privacy.Scalar).Sub(aL[i], new(privacy.Scalar).FromUint64(1))
+	}
+
+	// random alpha
+	alpha := privacy.RandomScalar()
+
+	// Commitment to aL, aR: A = h^alpha * G^aL * H^aR
+	A, err := encodeVectors(aL, aR, aggParam.g, aggParam.h)
+	if err != nil {
+		return nil, err
+	}
+	A.Add(A, new(privacy.Point).ScalarMult(privacy.PedCom.G[privacy.PedersenRandomnessIndex], alpha))
+	proof.a = A
+
+	// Random blinding vectors sL, sR
+	sL := make([]*privacy.Scalar, n*numValuePad)
+	sR := make([]*privacy.Scalar, n*numValuePad)
+	for i := range sL {
+		sL[i] = privacy.RandomScalar()
+		sR[i] = privacy.RandomScalar()
+	}
+
+	// random rho
+	rho := privacy.RandomScalar()
+
+	// Commitment to sL, sR : S = h^rho * G^sL * H^sR
+	S, err := encodeVectors(sL, sR, aggParam.g, aggParam.h)
+	if err != nil {
+		return nil, err
+	}
+	S.Add(S, new(privacy.Point).ScalarMult(privacy.PedCom.G[privacy.PedersenRandomnessIndex], rho))
+	proof.s = S
+
+	// challenge y, z
+	y := generateChallenge([][]byte{aggParam.cs, A.ToBytesS(), S.ToBytesS()})
+	z := generateChallenge([][]byte{aggParam.cs, A.ToBytesS(), S.ToBytesS(), y.ToBytesS()})
+
+	zNeg := new(privacy.Scalar).Sub(new(privacy.Scalar).FromUint64(0), z)
+	zSquare := new(privacy.Scalar).Mul(z, z)
+
+	// l(X) = (aL -z*1^n) + sL*X
+	yVector := powerVector(y, n*numValuePad)
+
+	l0 := vectorAddScalar(aL, zNeg)
+	l1 := sL
+
+	// r(X) = y^n hada (aR +z*1^n + sR*X) + z^2 * 2^n
+	hadaProduct, err := hadamardProduct(yVector, vectorAddScalar(aR, z))
+	if err != nil {
+		return nil, err
+	}
+
+	vectorSum := make([]*privacy.Scalar, n*numValuePad)
+	zTmp := new(privacy.Scalar).Set(z)
+	for j := 0; j < numValuePad; j++ {
+		zTmp.Mul(zTmp, z)
+		for i := 0; i < n; i++ {
+			vectorSum[j*n+i] = new(privacy.Scalar).Mul(twoVectorN[i], zTmp)
+		}
+	}
+
+	r0, err := vectorAdd(hadaProduct, vectorSum)
+	if err != nil {
+		return nil, err
+	}
+
+	r1, err := hadamardProduct(yVector, sR)
+	if err != nil {
+		return nil, err
+	}
+
+	//t(X) = <l(X), r(X)> = t0 + t1*X + t2*X^2
+
+	//calculate t0 = v*z^2 + delta(y, z)
+	deltaYZ := new(privacy.Scalar).Sub(z, zSquare)
+
+	// innerProduct1 = <1^(n*m), y^(n*m)>
+	innerProduct1 := new(privacy.Scalar).FromUint64(0)
+	for i := 0; i < n*numValuePad; i++ {
+		innerProduct1.Add(innerProduct1, yVector[i])
+	}
+
+	deltaYZ.Mul(deltaYZ, innerProduct1)
+
+	// innerProduct2 = <1^n, 2^n>
+	innerProduct2 := new(privacy.Scalar).FromUint64(0)
+	for i := 0; i < n; i++ {
+		innerProduct2.Add(innerProduct2, twoVectorN[i])
+	}
+
+	sum := new(privacy.Scalar).FromUint64(0)
+	zTmp = new(privacy.Scalar).Set(zSquare)
+	for j := 0; j < numValuePad; j++ {
+		zTmp.Mul(zTmp, z)
+		sum.Add(sum, zTmp)
+	}
+	sum.Mul(sum, innerProduct2)
+	deltaYZ.Sub(deltaYZ, sum)
+
+	// t1 = <l1, r0> + <l0, r1>
+	innerProduct3, err := innerProduct(l1, r0)
+	if err != nil {
+		return nil, err
+	}
+
+	innerProduct4, err := innerProduct(l0, r1)
+	if err != nil {
+		return nil, err
+	}
+
+	t1 := new(privacy.Scalar).Add(innerProduct3, innerProduct4)
+
+	// t2 = <l1, r1>
+	t2, err := innerProduct(l1, r1)
+	if err != nil {
+		return nil, err
+	}
+
+	// commitment to t1, t2
+	tau1 := privacy.RandomScalar()
+	tau2 := privacy.RandomScalar()
+
+	proof.t1 = privacy.PedCom.CommitAtIndex(t1, tau1, privacy.PedersenValueIndex)
+	proof.t2 = privacy.PedCom.CommitAtIndex(t2, tau2, privacy.PedersenValueIndex)
+
+	// challenge x = hash(G || H || A || S || T1 || T2)
+	x := generateChallenge([][]byte{aggParam.cs, proof.a.ToBytesS(), proof.s.ToBytesS(), proof.t1.ToBytesS(), proof.t2.ToBytesS()})
+
+	xSquare := new(privacy.Scalar).Mul(x, x)
+
+	// lVector = aL - z*1^n + sL*x
+	lVector, err := vectorAdd(vectorAddScalar(aL, zNeg), vectorMulScalar(sL, x))
+	if err != nil {
+		return nil, err
+	}
+
+	// rVector = y^n hada (aR +z*1^n + sR*x) + z^2*2^n
+	tmpVector, err := vectorAdd(vectorAddScalar(aR, z), vectorMulScalar(sR, x))
+	if err != nil {
+		return nil, err
+	}
+	rVector, err := hadamardProduct(yVector, tmpVector)
+	if err != nil {
+		return nil, err
+	}
+
+	vectorSum = make([]*privacy.Scalar, n*numValuePad)
+	zTmp = new(privacy.Scalar).Set(z)
+	for j := 0; j < numValuePad; j++ {
+		zTmp.Mul(zTmp, z)
+		for i := 0; i < n; i++ {
+			vectorSum[j*n+i] = new(privacy.Scalar).Mul(twoVectorN[i], zTmp)
+		}
+	}
+
+	rVector, err = vectorAdd(rVector, vectorSum)
+	if err != nil {
+		return nil, err
+	}
+
+	// tHat = <lVector, rVector>
+	proof.tHat, err = innerProduct(lVector, rVector)
+	if err != nil {
+		return nil, err
+	}
+
+	// blinding value for tHat: tauX = tau2*x^2 + tau1*x + z^2*rand
+	proof.tauX = new(privacy.Scalar).Mul(tau2, xSquare)
+	proof.tauX.Add(proof.tauX, new(privacy.Scalar).Mul(tau1, x))
+	zTmp = new(privacy.Scalar).Set(z)
+	tmpBN := new(privacy.Scalar)
+	for j := 0; j < numValuePad; j++ {
+		zTmp.Mul(zTmp, z)
+		proof.tauX.Add(proof.tauX, tmpBN.Mul(zTmp, rands[j]))
+	}
+
+	// alpha, rho blind A, S
+	// mu = alpha + rho*x
+	proof.mu = new(privacy.Scalar).Mul(rho, x)
+	proof.mu.Add(proof.mu, alpha)
+
+	// instead of sending left vector and right vector, we use inner sum argument to reduce proof size from 2*n to 2(log2(n)) + 2
+	innerProductWit := new(InnerProductWitness)
+	innerProductWit.a = lVector
+	innerProductWit.b = rVector
+	innerProductWit.p, err = encodeVectors(lVector, rVector, aggParam.g, aggParam.h)
+	if err != nil {
+		return nil, err
+	}
+	innerProductWit.p = innerProductWit.p.Add(innerProductWit.p, new(privacy.Point).ScalarMult(aggParam.u, proof.tHat))
+
+	proof.innerProductProof, err = innerProductWit.Prove(aggParam)
+	if err != nil {
+		return nil, err
+	}
+
 	return proof, nil
 }
 
